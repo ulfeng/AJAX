db6d2f96500bf93d118cd77c41be09038b260f97
sunshine9 <1104506375@qq.com>
2015-09-25 10:10:30 +0800

    一个简单的AJAX实例

---
+++ demo/01 一个简单的AJAX实例/JavaScript.js
@@ -0,0 +1,24 @@
+﻿function loadXMLDoc() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {  // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {  // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("demo").innerHTML = xmlhttp.responseText;
+        }
+    }
+
+    xmlhttp.open("GET", "Ajax_info.txt", true);
+    xmlhttp.send();
+}
+window.onload = function () {
+    var oBtn = document.getElementsByTagName("button")[0];
+    oBtn.onclick = function () {
+        loadXMLDoc();
+    }
+}
