f0d0820da0cefd1dbea68e485b67061c0164b13c
sunshine9 <1104506375@qq.com>
2015-09-25 10:40:26 +0800

    用AJAX 获得一次Header请求

---
+++ demo/03 用AJAX 获得一次Head请求/JavaScript.js
@@ -0,0 +1,26 @@
+﻿function loadXMLDoc() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {  // code for IE7+, Firefox, Google, Opera, Sarfri
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {  // code for IE6, IE5
+        xmlhttp = new ActiveXObject();
+    }
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("demo").innerHTML = xmlhttp.getAllResponseHeaders();
+        }
+    }
+
+    xmlhttp.open("GET", "Ajax_info.xml", true);
+    xmlhttp.send();
+
+}
+
+window.onload = function () {
+    var oBtn = document.getElementsByTagName("button")[0];
+    oBtn.onclick = function () {
+        loadXMLDoc();
+    }
+}
