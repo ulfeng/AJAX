d636a4919820bd226a1a013ce79d188666b29212
sunshine9 <1104506375@qq.com>
2015-09-25 10:26:32 +0800

    用 AJAX 加载XML文件

---
+++ demo/02 用AJAX 加载XML文件/JavaScript.js
@@ -0,0 +1,27 @@
+﻿function loadXMLDoc(url) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {  // code for IE7+,Firefox,Opera,Sarfri
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("demo1").innerHTML = xmlhttp.status;
+            document.getElementById("demo2").innerHTML = xmlhttp.statusText;
+            document.getElementById("demo3").innerHTML = xmlhttp.responseText;
+        }
+    }
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+window.onload = function () {
+    var oBtn = document.getElementsByTagName("button")[0];
+    oBtn.onclick = function () {
+        loadXMLDoc("Ajax_info.xml");
+    }
+}
